6b7e04581207781d9f98aad7eb07073f48e029b6
Tiagothyga <102805728+Tiagothyga@users.noreply.github.com>
2022-04-23 11:52:58 -0300

    Add files via upload

---
+++ Atividade04.py
@@ -0,0 +1,9 @@
+# obs usando os exeomlos dos links eu não consegui e pesquisando na internet achei o .capitalize() 
+nome = input("Digite o seu Nome:")
+dia = input("Digite o dia do seu nascimento:")
+mes = input("Digite o mês do seu nascimento:")
+ano = input("Digite o ano do seu nascimento:") 
+
+x = ( " nasceu no dia: " + dia + "/" + mes + "/" + ano)
+
+print( nome.capitalize() + x)
